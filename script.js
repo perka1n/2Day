6400a296de10193abba5b3432b6d61c3f00ecea6
perka1n <flowerdaner@gmail.com>
2023-05-18 15:55:34 +0400

    Added text_array

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let display = document.querySelector(".display")
 let text_array = ['Shadow Fiend', 'Phantom Assasin', 'AntiMage', 'Brew Master', 'Templar Assasin', 'Nyx Assasin', 'Lifestealer', 'Muerta', 'Primal Beast', 
 'Snapfire', 'Windtanger', 'Tiny', 'Beast Master', 'Pudge', 'Treant Protector', 'Oracle', 'Dazzle', 'Zeus', 'Legion Comannder', 'Jakiro']
 let style_array = ["color1", "color2", "color3", "color4", "color5", "color6", ]
+
+random_button.addEventListener("click", () => {
+    display.innerHTML = text_array[Math.floor(Math.random() * (text_array.length))]
+    display.className = style_array[Math.floor(Math.random() * style_array.length)]
+})
