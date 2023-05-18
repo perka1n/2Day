6fa91cffb92c823abf54bec9366d2677e1730ef9
perka1n <flowerdaner@gmail.com>
2023-05-18 15:54:43 +0400

    Added buttons in script

---
+++ script.js
@@ -0,0 +1,6 @@
+let random_button = document.querySelector(".random_button")
+let clear_button = document.querySelector(".clear_button")
+let display = document.querySelector(".display")
+let text_array = ['Shadow Fiend', 'Phantom Assasin', 'AntiMage', 'Brew Master', 'Templar Assasin', 'Nyx Assasin', 'Lifestealer', 'Muerta', 'Primal Beast', 
+'Snapfire', 'Windtanger', 'Tiny', 'Beast Master', 'Pudge', 'Treant Protector', 'Oracle', 'Dazzle', 'Zeus', 'Legion Comannder', 'Jakiro']
+let style_array = ["color1", "color2", "color3", "color4", "color5", "color6", ]
